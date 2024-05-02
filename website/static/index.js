1da52c5954b7388c1d10ea062de16c1018bc21a0
gezzyz <139928315+gezzyz@users.noreply.github.com>
2024-05-02 15:44:55 -0300

    Finished template

---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteId){
+    fetch('/delete-node',{
+        method: 'POST',
+        body: JSON.stringify({noteId:noteId})
+    }).then((_res) => {
+        window.location.href="/";
+    });
+}
