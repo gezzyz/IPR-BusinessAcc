b50b1675c262dde551ae2fc3a4322f591472a946
gezzyz <139928315+gezzyz@users.noreply.github.com>
2024-05-09 20:14:00 -0300

    Deleted old models

--- website/view.py
+++ website/view.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note, Account
+from .models import Account
 from . import db
 import json
 
